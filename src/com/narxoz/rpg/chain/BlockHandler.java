8e9c0eac77e47ea25b049ea55829e38b13b47268
gulnar-gif <gulnar.khabibullayeva@narxoz.kz>
2026-04-02 21:50:08 +0500

    HW6

--- src/com/narxoz/rpg/chain/BlockHandler.java
+++ src/com/narxoz/rpg/chain/BlockHandler.java
@@ -11,10 +11,15 @@ public class BlockHandler extends DefenseHandler {
 
     @Override
     public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Calculate how much damage is blocked: (int)(incomingDamage * blockPercent).
-        // TODO: Subtract the blocked amount from incomingDamage to get the remainder.
-        // TODO: Print a block message showing how much was blocked.
-        // TODO: Always pass the remainder to the next handler (block reduces but never stops the chain).
-        // Design question: what should happen if the remainder reaches 0 or below?
+        int blocked = (int)(incomingDamage * blockPercent);
+
+        int remainingDamage = incomingDamage - blocked;
+
+        if (remainingDamage < 0) {
+            remainingDamage = 0;
+        }
+
+        System.out.println("Block reduced: " + blocked + " damage");
+        passToNext(remainingDamage, target);
     }
 }
